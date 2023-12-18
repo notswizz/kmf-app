35afe80fabc570c3eb18b233c0e45be710a8c652
Your Name <you@example.com>
2023-12-17 21:27:57 -0500

    fixed picmodal import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/navBar'; 
-import PicModal from '../components/PicModal';
+import PicModal from '../components/picModal';
 
 
 const IndexPage = () => {
