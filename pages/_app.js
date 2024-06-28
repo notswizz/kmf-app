009ce5328128fd76905e224dde082e6b9a55acd9
swizz <emailswizz@gmail.com>
2024-06-27 20:18:54 -0400

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import Head from 'next/head';
 import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=375, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <Component {...pageProps} />
+      <Analytics/>
     </>
   );
 }
